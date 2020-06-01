cccb3de5de072626df5c0bda3657df5ddb30c22a
isrnao <jggpad@gmail.com>
2020-06-02 04:54:22 +0900

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -41,8 +41,8 @@ function createCalendar(year, month) {
                 calendarHtml += '<td class="is-disabled">' + num + '</td>';
                 dayCount++;
             } else {
-                    if (!(dayCount>=date.getDate()) && month==date.getMonth()+1 && year==date.getFullYear()) calendarHtml += `<td class="calendar_td" id=${dayCount} >-</td>`;
-                    else calendarHtml += `<td class="calendar_td" id=${dayCount} data-date='${year}-${('00'+month).slice( -2 )}-${('00'+dayCount).slice( -2 )}'>${dayCount}</td>`;
+                    if (!(dayCount >= date.getDate()) && month == date.getMonth() + 1 && year == date.getFullYear()) calendarHtml += `<td class="calendar_td" id=${('00'+dayCount).slice( -2 )} >-</td>`;
+                    else calendarHtml += `<td class="calendar_td" id=${('00'+dayCount).slice( -2 )} data-date='${year}-${('00'+month).slice( -2 )}-${('00'+dayCount).slice( -2 )}'>${dayCount}</td>`; 
                 dayCount++;
             };
         };
@@ -54,7 +54,7 @@ function createCalendar(year, month) {
 
 function moveCalendar(e) {
     document.querySelector('#calendar').innerHTML = ''; //表示カレンダー初期化
-    if (e.target.id === 'prev') {
+    if (e.target.id === 'prev' && month != date.getMonth() + 1) {
         month--;
         if (month < 1) {
             year--;
@@ -77,13 +77,16 @@ document.querySelector('#next').addEventListener('click', moveCalendar);
 document.addEventListener("click", function(e) {
     if(e.target.classList.contains("calendar_td")) {
         const dayColor = document.getElementById( e.target.dataset.date.slice(8,10) );
-            endDayNum = createCalendar(e.target.dataset.date.slice(0,4),e.target.dataset.date.slice(5,7))
-            for(var i=1;i<Number(endDayNum[1]);++i){
-            document.getElementById( i ).style.backgroundColor='white';
-            };
-            document.getElementById( e.target.dataset.date.slice(8,10) ).style.backgroundColor='pink';
-            document.getElementById( "target" ).value = e.target.dataset.date
-            alert('予約日：' + e.target.dataset.date);
+        endDayNum = createCalendar(e.target.dataset.date.slice(0,4),e.target.dataset.date.slice(5,7))    
+        for (var i=1; i<=Number(endDayNum[1]); ++i) {
+            document.getElementById(('00'+i).slice( -2 )).style.backgroundColor = 'white';
+        };
+        document.getElementById(e.target.dataset.date.slice(8, 10)).style.backgroundColor = 'pink';
+        document.getElementById("target").value = e.target.dataset.date
+        alert('予約日：' + e.target.dataset.date);
     }
 });
 showCalendar(year, month);
+const nowDate = new Date()
+document.getElementById(('00'+nowDate.getDate()).slice( -2 )).style.backgroundColor = 'pink';
+document.getElementById("target").value = nowDate.getFullYear() + "-" + (nowDate.getMonth() + 1) + "-" + nowDate.getDate();
